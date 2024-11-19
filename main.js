543c6998fb7606994ad7257635604480779ed7d6
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-18 23:59:36 -0500

    meets all deliverables

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// grabbing some elements from the DOM
 
+const likes = document.querySelectorAll('.like');
+const hearts = document.querySelectorAll('.like-glyph');
+const modal = document.getElementById('modal');
 
+// some functions
+
+const toggleHeart = (heart) => {
+  mimicServerCall()
+    .then(resp => {
+      if (resp) {
+        if (heart.classList.contains('activated-heart')) {
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        } else {
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        }
+      }
+    })
+    .catch(error => {
+      modal.classList.remove('hidden');
+      setTimeout(() => modal.classList.add('hidden'), 3000);
+    });
+};
+const click = element => element.addEventListener('click', function() {
+  toggleHeart(this.firstElementChild)
+});
+
+// add click functionality to each like button
+
+likes.forEach(element => click(element));
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
